7d56c892115be791f4dd5329c5b552b63d7cfbf5
Arjen Schwarz <developer@arjen.eu>
2025-09-10 17:40:44 +1000

    Use the tool to make decisions

--- agents/4. mcp-agent/mcp_agent.py
+++ agents/4. mcp-agent/mcp_agent.py
@@ -1,17 +1,31 @@
 from bedrock_agentcore import BedrockAgentCoreApp
+from strands.tools.mcp.mcp_client import MCPClient
+from strands.tools.mcp.transport import streamablehttp_client
 from strands import Agent
 
 SYSTEM_PROMPT = """
 You are a game master in a fantasy setting. In this world called Bedrock, there are people with special powers called agents. These agents will have an elemental power that they use to fight against an incursion of demonic entities.
 The player is one of these agents and will state what element their powers have. During the game they will evolve these powers to be able to do more, but initially they'll be restricted to small abilities (light a finger on fire, turn a finger into rock, create light air pressure, etc. You get to tell them their initial power and will decide when they've reached the point where they will evolve to more powerful powers.
-At the start of the game, the player is dropped into a labyrinth where they need to find the exit. You will ask for their name, power and what direction they want to go. There will be dead ends in the path, as well as places where they find enemies or treasures that will boost their powers. When enemies are found, there needs to be a battle that should be solvable with the players current skill level. Reward creativity in this regard.
+At the start of the game, the player is dropped into a labyrinth where they need to find the exit. You will ask for their name, power and what direction they want to go. There will be dead ends in the path, as well as places where they find enemies or treasures that will boost their powers. To decide what the player finds, use the decide_path tool When enemies are found, there needs to be a battle that should be solvable with the players current skill level. Reward creativity in this regard.
 The exit should be found within 20 moves of the player starting.
 IMPORTANT: The world of Bedrock is a civilised place where British English is being used.
 """
 
+
+def create_streamable_http_transport():
+    return streamablehttp_client(
+        gatewayURL, headers={"Authorization": f"Bearer {token}"}
+    )
+
+
+client = MCPClient(create_streamable_http_transport)
+
+tools = client.list_tools_sync()
+
 app = BedrockAgentCoreApp()
 agent = Agent(
     model="amazon.nova-lite-v1:0",
+    tools=tools,
     system_prompt=SYSTEM_PROMPT)
 
 @app.entrypoint
